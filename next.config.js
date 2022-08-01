907e41c6bb9bcf404c9810f1208ba82363721575
Galeno <thiago.galeno@wivenn.com.br>
2022-08-01 16:01:59 -0300

    feat: files conig, axios lib

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['https://st.depositphotos.com/'],
+    domains: ['st.depositphotos.com', 'depositphotos.com', 'www.st.depositphotos.com', 'www.depositphotos.com', 'https://st.depositphotos.com/'],
   },
 }
 
