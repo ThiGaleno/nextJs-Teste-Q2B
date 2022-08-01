b4bd3615973e0d95dcf387acdc0c7756091d7992
Galeno <thiago.galeno@wivenn.com.br>
2022-08-01 16:04:00 -0300

    feat: authenticate permissions and header component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Header from '../src/components/Header'
+import { useAuth } from '../hooks/useAuth';
+import { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { isLoggedIn } = useAuth();
+
+  const [isLogged, setIsLogged] = useState(false);
+
+  useEffect(() => {
+    setIsLogged(isLoggedIn());
+  }, [isLoggedIn])
+  return (
+    <>
+    {
+      isLogged === true ? <Header /> : null
+    }
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import Router from 'next/router';
+import React, { useEffect } from 'react'
+import { useAuth } from '../../hooks/useAuth';
+
+const Header = () => {
+    const { username, logout } = useAuth();
+
+    return (
+        <header className='p-24 flex justify-between'>
+            <div className='subtitle-text'>
+                <p>Seja bem vindo <strong className='text-primary'>{username || ''}</strong></p>
+            </div>
+            <a onClick={logout}>Sair</a>
+        </header>
+    )
+}
+
+export default Header
